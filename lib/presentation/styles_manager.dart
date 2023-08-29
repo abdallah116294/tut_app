0c5101548d3246b13af832b1e04a11a849962f23
abdallah <abdallhamohamed116@gmail.com>
2023-08-29 10:41:12 +0300

    values & styles manager

---
+++ lib/presentation/styles_manager.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:learn_clean_arch/presentation/font_manger.dart';
+
+TextStyle _getTextStyle(double fontSize, FontWeight fontWeight, Color color) {
+  return TextStyle(
+      fontSize: fontSize,
+      fontFamily: FontManager.fontFamily,
+      color: color,
+      fontWeight: fontWeight);
+}
+
+TextStyle getRegularStyle(double fontSize, Color color) {
+  return _getTextStyle(fontSize, FontWeightManager.regular, color);
+}
+
+TextStyle getMediumStyle(double fontSize, Color color) {
+  return _getTextStyle(fontSize, FontWeightManager.medium, color);
+}
+TextStyle getLightStyle(double fontSize, Color color) {
+  return _getTextStyle(fontSize, FontWeightManager.light, color);
+}
+TextStyle getSemiBoldStyle(double fontSize, Color color) {
+  return _getTextStyle(fontSize, FontWeightManager.semiBold, color);
+}
+TextStyle getBoldStyle(double fontSize, Color color) {
+  return _getTextStyle(fontSize, FontWeightManager.bold, color);
+}
