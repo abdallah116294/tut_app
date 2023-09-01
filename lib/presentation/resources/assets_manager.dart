405ac1e64eb5bc0277801e96e7decf2e9a2f2b9a
abdallah <abdallhamohamed116@gmail.com>
2023-09-01 19:34:32 +0300

    started with onboarding screen

--- lib/presentation/resources/assets_manager.dart
+++ lib/presentation/resources/assets_manager.dart
@@ -1,5 +1,9 @@
-const String IMAGE_PATH = "assets/images";
+const String imagePath = "assets/images";
 
 class ImageAssets {
-  static const String splashImage = "$IMAGE_PATH/splash_logo.png";
+  static const String splashImage = "$imagePath/splash_logo.png";
+  static const String onBoardingImage1 = "$imagePath/onboarding1.svg";
+  static const String onBoardingImage2 = "$imagePath/onboarding2.svg";
+  static const String onBoardingImage3 = "$imagePath/onboarding3.svg";
+  static const String onBoardingImage4 = "$imagePath/onboarding4.svg";
 }
--- lib/presentation/resources/strings_manager.dart
+++ lib/presentation/resources/strings_manager.dart
@@ -1,3 +1,14 @@
-class AppStringManager{
-  
+class AppStringManager {
+  static const String onBoardingTitle1 = "SEE THE BEST COURSE #1";
+  static const String onBoardingSubtilte1 =
+      "Tut app is an awesome flutter application using clean architecture #1";
+  static const String onBoardingTitle2 = "SEE THE BEST COURSE #2";
+  static const String onBoardingSubtitle2 =
+      "Tut app is an awesome flutter application using clean architecture #2";
+  static const String onBoardingTitle3 = "SEE THE BEST COURSE #3";
+  static const String onBoardingSubTitle3 =
+      "Tut app is an awesome flutter application using clean architecture #3";
+        static const String onBoardingTitle4 = "SEE THE BEST COURSE #4";
+  static const String onBoardingSubTitle4 =
+      "Tut app is an awesome flutter application using clean architecture #4";
 }
